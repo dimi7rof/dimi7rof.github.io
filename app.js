14c12a7b7fa7ec57f93a18fd975cbde523081ef9
dimi7rof <84029988+dimi7rof@users.noreply.github.com>
2023-04-14 18:16:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let certBtn = document.getElementById('certBtn');
 certBtn.addEventListener('click', showCertificates);
+let certBtn2 = document.getElementById('certBtn2');
+// certBtn.addEventListener('click', showCertificates);
+
 
 function showCertificates() {
     let div = document.getElementById('cert');
     div.style.display = 'flex';
+    certBtn2.style.display = 'none';
 }
