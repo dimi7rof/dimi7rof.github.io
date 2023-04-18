ae208fdcc875bc1e4556636440a6361416829399
dimi7rof <84029988+dimi7rof@users.noreply.github.com>
2023-04-18 23:51:44 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-let certBtn = document.getElementById('certBtn');
-certBtn.addEventListener('click', showCertificates);
-let certBtn2 = document.getElementById('certBtn2');
-// certBtn.addEventListener('click', showCertificates);
-
-
 function showCertificates() {
     let div = document.getElementById('cert');
     div.style.display = 'flex';
