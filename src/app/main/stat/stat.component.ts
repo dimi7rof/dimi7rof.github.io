31911d7d96e6fedb78017f3928f90697e95d20db
dimi7 <dimi7rof@gmail.com>
2025-02-22 17:46:48 +0200

    feat: table for stats

--- src/app/main/stat/stat.component.ts
+++ src/app/main/stat/stat.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { interval } from 'rxjs';
+
+interface UserData {
+  ip: string;
+  os: string;
+  browser: string;
+  dateTime: string;
+}
 
 @Component({
   selector: 'stat-component',
@@ -7,17 +15,36 @@ import { UserService } from '../services/user.service';
   imports: [],
   template: `
     <section class="wrapper stat">
-      <header class="inner">
-        <p>{{ data }}</p>
-      </header>
+      <table class="user-table">
+        <thead>
+          <tr>
+            <th>IP Address</th>
+            <th>Operating System</th>
+            <th>Browser</th>
+            <th>Date & Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for (user of data; track user.dateTime) {
+          <tr>
+            <td>{{ user.ip }}</td>
+            <td>{{ user.os }}</td>
+            <td>{{ user.browser }}</td>
+            <td>{{ user.dateTime }}</td>
+          </tr>
+          }
+        </tbody>
+      </table>
     </section>
   `,
 })
 export class StatisticsComponent {
-  data = '';
+  data: UserData[] = [];
   constructor(private userService: UserService) {
-    this.userService.getStat().subscribe((data) => {
-      this.data = data;
-    });
+    interval(5000).subscribe(() =>
+      this.userService.getStat().subscribe((data: UserData[]) => {
+        this.data = data;
+      })
+    );
   }
 }
