55a0108220a45b17688d39057651582bf125dc90
dimi7rof <84029988+dimi7rof@users.noreply.github.com>
2025-03-09 09:21:31 +0200

    Update visitors.component.ts

--- src/app/main/hidden/visitors.component/visitors.component.ts
+++ src/app/main/hidden/visitors.component/visitors.component.ts
@@ -25,7 +25,7 @@ interface UserData {
 export class VisitorsComponent implements OnDestroy {
   data: UserData[] = [];
   private subscription: Subscription = new Subscription();
-  exclude = true;
+  exclude = false;
   ip = '0.0.0.0';
   page = 1;
   pageSize = environment.pageSize;
