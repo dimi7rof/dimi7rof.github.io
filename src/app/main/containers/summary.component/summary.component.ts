a93eb4a95ac7b83d758e0085db49e16637e6dde8
dimi7 <dimi7rof@gmail.com>
2025-07-13 14:18:14 +0300

    refactor:

--- src/app/main/containers/summary.component/summary.component.ts
+++ src/app/main/containers/summary.component/summary.component.ts
@@ -4,7 +4,18 @@ import { Component } from '@angular/core';
   selector: 'summary-component',
   standalone: true,
   imports: [],
-  templateUrl: './summary.component.html',
-  styleUrl: './summary.component.css',
+  template: ` <section class="summary wrapper box right">
+    <div class="text drop flex center column">
+      <h3>Summary</h3>
+      <p>Hello</p>
+      <p>I am <b>not</b> looking for a job</p>
+      <p>This page is for practice only</p>
+    </div>
+  </section>`,
+  styles: `
+.summary {
+  height: 350px;
+}
+`,
 })
 export class SummaryComponent {}
